d93d43b5773a6545c338c4454f90d4e695ba178e
Dana299 <gorevadana@gmail.com>
2023-01-13 16:42:00 +0300

    feat: add missing models and some fields to admin pannel

--- recipe_api/admin.py
+++ recipe_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Comment, Ingredient, Recipe, RecipeIngredient, RecipeStep
+from .models import (Comment, Image, Ingredient, Recipe, RecipeIngredient,
+                     RecipeStep)
 
 
 class CommentAdmin(admin.ModelAdmin):
@@ -13,7 +14,8 @@ class RecipeAdmin(admin.ModelAdmin):
         'author',
         'category',
         'get_no_ingredients',
-        'time_created'
+        'time_created',
+        'status',
     )
 
     def get_no_ingredients(self, obj):
@@ -27,11 +29,20 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
 
 
 class RecipeStepAdmin(admin.ModelAdmin):
-    list_display = ('recipe', 'image',)
+    list_display = ('recipe', 'step_text', 'image',)
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'get_image_url', 'is_temporary')
+
+    def get_image_url(self, obj):
+        return obj.image.url
+    get_image_url.short_description = "URL from storage"
 
 
 admin.site.register(Comment, CommentAdmin)
-admin.site.register(Ingredient)
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
 admin.site.register(RecipeStep, RecipeStepAdmin)
+admin.site.register(Ingredient)
