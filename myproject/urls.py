3928b5edc01b75870c9c7b3132047ddd3c34e562
Dana299 <gorevadana@gmail.com>
2023-03-07 14:20:34 +0300

    style: add missing line breaks

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,18 +7,28 @@ from drf_yasg.views import get_schema_view
 
 # swagger
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-   ),
-   public=True,
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+    ),
+    public=True,
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
     path("admin/", admin.site.urls),
     path("api/", include("recipe_api.urls", namespace="recipe_api")),
     path("api/", include("account.urls", namespace="account")),
@@ -28,5 +38,5 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
